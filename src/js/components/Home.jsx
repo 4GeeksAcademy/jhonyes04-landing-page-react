977f201d3b86a5f2280f5181a99bb93181adff8b
jhonyes04 <jhonyes@hotmail.com>
2026-02-07 11:33:20 +0100

    Solucionado problema con Home.jsx

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 //include images into your bundle
 import rigoImage from '../../img/rigo-baby.jpg';
 import { Navbar } from './Navbar/Navbar';
+import { Jumbotron } from './Jumbotron';
+import { Cards } from './Card/Cards';
+import { Footer } from './Footer';
 
 //create your first component
 const Home = () => {
